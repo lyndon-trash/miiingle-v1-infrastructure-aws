d92f801b3f9141dc56be61e5287192d0c8c31840
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-05-17 13:28:07 +0800

    Attempt to create a K8 cluster

--- kubernetes-cluster.tf
+++ kubernetes-cluster.tf
@@ -1,28 +1,32 @@
-//TODO:
-//follow this guide
-//https://github.com/hashicorp/learn-terraform-provision-eks-cluster
 
-data "aws_subnet_ids" "private_subnets" {
-  vpc_id = aws_vpc.main
+module "eks" {
+  source       = "terraform-aws-modules/eks/aws"
+  cluster_name = var.eks_cluster_name
+  vpc_id       = aws_vpc.main.id
+  subnets      = aws_subnet.private.*.id
 
-  filter {
-    name   = "tag:SubnetType"
-    values = ["private"]
-  }
+  tags = local.common_tags
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.small"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_default_security_group.default.id]
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "t2.medium"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_default_security_group.default.id]
+    }
+  ]
 }
 
-resource "aws_eks_cluster" "main" {
-  name     = "main-cluster"
-  role_arn = "arn:aws:iam::xx:role/xx"
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
 
-  vpc_config {
-    subnet_ids = data.aws_subnet_ids.private_subnets
-  }
-
-  tags = merge(
-    {
-      Name = format("%s-Main-EKS-Cluster", local.vpc_name)
-    },
-    local.common_tags
-  )
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
 }
--- main.tf
+++ main.tf
@@ -36,10 +36,38 @@ variable "bastion_source_ips" {
 variable "bastion_public_key" {
   description = "The Public Key with access to the bastion host"
   type        = string
-  default     = "ssh-rsa <insert public key here> <user>@<machine>"
+}
+
+variable "eks_cluster_name" {
+  description = "EKS Cluster name"
+  type        = string
+  default     = "terraform-cluster"
 }
 
 provider "aws" {
   version = "~> 2.0"
   region  = var.aws_region
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+provider "random" {
+  version = "~> 2.1"
+}
+
+provider "local" {
+  version = "~> 1.2"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
 }
