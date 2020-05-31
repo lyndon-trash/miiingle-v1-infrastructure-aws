09cea5656b14f77824112f1f862b6863c04614bd
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-06-01 00:29:19 +0800

    Improve the LB targeting by adding more tags

--- api_gateway.tf
+++ api_gateway.tf
@@ -2,10 +2,15 @@
 //once we figure out a way to run our helm charts inside as part of TF
 //this is a very unsuitable workaround, this will obviously break apart
 //when we start creating multiple instances of the environment
-data "aws_lb" "prod" {
-  tags = {
-    "kubernetes.io/service-name": "default/backend"
-  }
+data "aws_lb" "eks_internal" {
+  depends_on = [helm_release.backend]
+
+  tags = merge(
+    {
+      "kubernetes.io/service-name" : "default/backend"
+    },
+    local.common_tags
+  )
 }
 
 resource "aws_api_gateway_rest_api" "main" {
@@ -17,21 +22,19 @@ resource "aws_api_gateway_rest_api" "main" {
   }
 }
 
-resource "aws_api_gateway_vpc_link" "prod" {
-  depends_on = [helm_release.backend]
-
+resource "aws_api_gateway_vpc_link" "eks_internal" {
   name        = format("%s Link", local.vpc_name)
   description = format("%s Link for API Gateway to EKS", local.vpc_name)
-  target_arns = [data.aws_lb.prod.arn]
+  target_arns = [data.aws_lb.eks_internal.arn]
 
   tags = local.common_tags
 }
 
 resource "aws_api_gateway_method" "root_get" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_rest_api.main.root_resource_id
+  http_method   = "GET"
   authorization = "NONE"
-  http_method = "GET"
-  resource_id = aws_api_gateway_rest_api.main.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.main.id
 }
 
 resource "aws_api_gateway_integration" "root_get_integration" {
@@ -39,18 +42,19 @@ resource "aws_api_gateway_integration" "root_get_integration" {
   resource_id = aws_api_gateway_rest_api.main.root_resource_id
   http_method = "GET"
 
-  connection_type         = "VPC_LINK"
-  connection_id           = aws_api_gateway_vpc_link.prod.id
+  connection_type = "VPC_LINK"
+  connection_id   = aws_api_gateway_vpc_link.eks_internal.id
 
   type                    = "HTTP"
-  uri                     = "http://${data.aws_lb.prod.dns_name}/"
+  uri                     = "http://${data.aws_lb.eks_internal.dns_name}/"
   integration_http_method = "GET"
 }
 
-resource "aws_api_gateway_deployment" "prod" {
+resource "aws_api_gateway_deployment" "root_get_integration" {
   depends_on  = [aws_api_gateway_integration.root_get_integration]
   rest_api_id = aws_api_gateway_rest_api.main.id
   stage_name  = "prod"
+  stage_description = "The Prod API"
 }
 
 
--- helm.tf
+++ helm.tf
@@ -1,36 +1,40 @@
 resource "kubernetes_service_account" "tiller" {
   depends_on = [module.eks_cluster]
+
   metadata {
-    name = "tiller"
+    name      = "tiller"
     namespace = "kube-system"
   }
 }
 
 resource "kubernetes_cluster_role_binding" "tiller" {
   depends_on = [module.eks_cluster]
+
   metadata {
     name = "tiller"
   }
+
   role_ref {
     api_group = "rbac.authorization.k8s.io"
-    kind = "ClusterRole"
-    name = "cluster-admin"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
   }
+
   subject {
-    kind = "ServiceAccount"
+    kind      = "ServiceAccount"
     namespace = "kube-system"
-    name = "tiller"
+    name      = "tiller"
   }
 }
 
 resource "helm_release" "kube-utils" {
   depends_on = [kubernetes_cluster_role_binding.tiller, kubernetes_service_account.tiller]
-  chart = "../infrastructure-helm/kube-utils"
-  name = "kube-utils"
+  chart      = "../infrastructure-helm/kube-utils"
+  name       = "kube-utils"
 }
 
 resource "helm_release" "backend" {
   depends_on = [helm_release.kube-utils]
-  chart = "../infrastructure-helm/backend"
-  name = "backend"
+  chart      = "../infrastructure-helm/backend"
+  name       = "backend"
 }
