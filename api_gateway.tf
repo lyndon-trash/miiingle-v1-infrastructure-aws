821a96ad007c1a22fbc4eaf551561a15bb2a6d81
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-06-01 10:22:55 +0800

    Add a better way to easily import the open-api configuration

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,3 +1,17 @@
+data "template_file" "api_specs" {
+  template = file("open-api.yaml")
+}
+
+resource "aws_api_gateway_rest_api" "main" {
+  name        = "Miiingle.NET API"
+  description = "The API"
+  body        = data.template_file.api_specs.rendered
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
 //currently, we are creating the LB outside of TF context
 //once we figure out a way to run our helm charts inside as part of TF
 //this is a very unsuitable workaround, this will obviously break apart
@@ -13,47 +27,50 @@ data "aws_lb" "eks_internal" {
   )
 }
 
-resource "aws_api_gateway_rest_api" "main" {
-  name        = "Miiingle.NET API"
-  description = "The API"
-
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
-
 resource "aws_api_gateway_vpc_link" "eks_internal" {
   name        = format("%s Link", local.vpc_name)
   description = format("%s Link for API Gateway to EKS", local.vpc_name)
   target_arns = [data.aws_lb.eks_internal.arn]
 
   tags = local.common_tags
+
+  lifecycle {
+    ignore_changes = [
+      //this is a workaround. i have no clue why this list refreshes
+      target_arns
+    ]
+  }
 }
 
-resource "aws_api_gateway_method" "root_get" {
-  rest_api_id   = aws_api_gateway_rest_api.main.id
-  resource_id   = aws_api_gateway_rest_api.main.root_resource_id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "root_get_integration" {
+data "aws_api_gateway_resource" "registrations" {
   rest_api_id = aws_api_gateway_rest_api.main.id
-  resource_id = aws_api_gateway_rest_api.main.root_resource_id
+  path        = "/registrations"
+}
+
+resource "aws_api_gateway_integration" "registrations_get" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = data.aws_api_gateway_resource.registrations.id
   http_method = "GET"
 
   connection_type = "VPC_LINK"
   connection_id   = aws_api_gateway_vpc_link.eks_internal.id
 
   type                    = "HTTP"
-  uri                     = "http://${data.aws_lb.eks_internal.dns_name}/"
+  uri                     = "http://${data.aws_lb.eks_internal.dns_name}/registrations"
   integration_http_method = "GET"
 }
 
+resource "aws_api_gateway_integration_response" "registrations_get" {
+  rest_api_id = aws_api_gateway_integration.registrations_get.rest_api_id
+  resource_id = aws_api_gateway_integration.registrations_get.resource_id
+  http_method = "GET"
+  status_code = "200"
+}
+
 resource "aws_api_gateway_deployment" "root_get_integration" {
-  depends_on  = [aws_api_gateway_integration.root_get_integration]
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  stage_name  = "prod"
+  depends_on        = [aws_api_gateway_vpc_link.eks_internal, aws_api_gateway_integration.registrations_get]
+  rest_api_id       = aws_api_gateway_rest_api.main.id
+  stage_name        = "prod"
   stage_description = "The Prod API"
 }
 
