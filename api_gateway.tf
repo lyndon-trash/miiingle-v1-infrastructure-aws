43d68701a80ee85d055f283e0bd99e69a89e7076
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-06-03 05:55:31 +0800

    WIP: Integration partially working

--- api_gateway.tf
+++ api_gateway.tf
@@ -34,6 +34,11 @@ data "aws_lb" "eks_internal" {
   )
 }
 
+data "aws_lb_listener" "eks_internal" {
+  load_balancer_arn = data.aws_lb.eks_internal.arn
+  port              = 80
+}
+
 //sometime this resource gets problematic, this happens when aws_lb.eks_internal is not yet ready
 //TODO: create this resource only after aws_lb is ready to serve
 //resource "aws_api_gateway_vpc_link" "eks_internal" {
@@ -52,7 +57,7 @@ data "aws_lb" "eks_internal" {
 //}
 resource "aws_apigatewayv2_vpc_link" "eks_internal" {
   name               = format("%s Link", local.vpc_name)
-  security_group_ids = [aws_default_security_group.default]
+  security_group_ids = [aws_default_security_group.default.id]
   subnet_ids         = aws_subnet.private.*.id
 
   tags = local.common_tags
@@ -63,9 +68,11 @@ resource "aws_apigatewayv2_vpc_link" "eks_internal" {
 //  path        = "/registrations"
 //}
 resource "aws_apigatewayv2_route" "registrations_get" {
+  depends_on = [helm_release.backend]
+
   api_id         = aws_apigatewayv2_api.main.id
-  route_key      = "/registrations"
-  operation_name = "GET"
+  route_key      = "GET /registrations"
+  operation_name = "List of Registrations"
 }
 
 //resource "aws_api_gateway_integration" "registrations_get" {
@@ -81,15 +88,18 @@ resource "aws_apigatewayv2_route" "registrations_get" {
 //  integration_http_method = "GET"
 //}
 resource "aws_apigatewayv2_integration" "registrations_get" {
+  depends_on = [helm_release.backend]
+
   api_id      = aws_apigatewayv2_api.main.id
   description = "List all the registration"
 
   connection_type = "VPC_LINK"
   connection_id   = aws_apigatewayv2_vpc_link.eks_internal.id
 
-  integration_type   = "HTTP"
+  integration_type   = "HTTP_PROXY"
   integration_method = "GET"
-  integration_uri    = "http://${data.aws_lb.eks_internal.dns_name}/registrations"
+  integration_uri    = data.aws_lb_listener.eks_internal.arn
+  //  integration_uri    = "http://${data.aws_lb.eks_internal.dns_name}/registrations"
 }
 
 //resource "aws_api_gateway_integration_response" "registrations_get" {
