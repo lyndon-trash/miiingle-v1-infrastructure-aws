544e0c5522daadd430a4739d026521e27bef2358
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-06-10 11:35:49 +0800

    Add cognito client

--- cognito.tf
+++ cognito.tf
@@ -1,3 +1,8 @@
 resource "aws_cognito_user_pool" "main" {
   name = "prod_users"
 }
+
+resource "aws_cognito_user_pool_client" "client" {
+  name         = "client-web"
+  user_pool_id = aws_cognito_user_pool.main.id
+}
