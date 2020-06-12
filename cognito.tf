442cc30978ea08c2bc40f3fe567ed9180aedb19a
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-06-12 23:12:13 +0800

    Not sure if this actually worked, but somehow my auth went through

--- cognito.tf
+++ cognito.tf
@@ -5,10 +5,37 @@ resource "aws_cognito_user_pool" "main" {
 resource "aws_cognito_user_pool_client" "client" {
   name         = "client-web"
   user_pool_id = aws_cognito_user_pool.main.id
+
+  prevent_user_existence_errors = "LEGACY"
+  read_attributes               = []
+  refresh_token_validity        = 30
+
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows                  = ["code"]
+
+  supported_identity_providers = ["COGNITO"]
+
+  allowed_oauth_scopes = [
+    "aws.cognito.signin.user.admin",
+    "email",
+    "https://api.${var.domain_base}/backend",
+    "openid",
+    "phone",
+    "profile",
+  ]
+
+  explicit_auth_flows = [
+    "ALLOW_CUSTOM_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_SRP_AUTH",
+  ]
+
+  logout_urls   = ["https://localhost:4200/logout"]
+  callback_urls = ["https://localhost:4200/auth"]
 }
 
 resource "aws_cognito_resource_server" "resource" {
-  identifier   = "https://api.miiingle.net"
+  identifier   = "https://api.${var.domain_base}"
   name         = "backend"
   user_pool_id = aws_cognito_user_pool.main.id
 
